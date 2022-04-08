8c69fdab57c7ef4c78acb1956be53104a2cffb95
Mohamedboulj <med.blj93@gmail.com>
2022-04-08 15:08:41 +0000

    First commit

--- src/index.js
+++ src/index.js
@@ -1,11 +1,44 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, { useState } from "react";
+import { createRoot } from "react-dom/client";
 
-const element = <h1>Hello, world</h1>;
+function Addition() {
+  const [Val1, setVal1] = useState(0);
+  const [Val2, setVal2] = useState(0);
+  const [Sum, setSum] = useState();
 
-ReactDOM.render(
-( element,
-  document.getElementById('root'))
-);
+  return (
+    <div>
+      <input
+        type="text"
+        onInput={(e) => {
+          setVal1(Number(e.target.value));
+        }}
+      />
+      &nbsp; 
+      +
+      &nbsp; 
+      <input
+        type="text"
+        onInput={(e) => {
+          setVal2(Number(e.target.value));
+        }}
+      />
+      <button
+        onClick={() => {
+          setSum(Val1 + Val2);
+        }}
+      >
+        Calculer!
+      </button>
+      &nbsp; 
 
+      =
+      &nbsp; 
 
+      <label>{Sum}</label>
+      
+    </div>
+  );
+}
+
+createRoot(document.getElementById("root")).render(<Addition />);
