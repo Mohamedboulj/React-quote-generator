8ab931f3494f51c2a65791ed843c126bf8ba3020
Mohamedboulj <med.blj93@gmail.com>
2022-04-09 18:58:39 +0000

    First commit

--- src/index.js
+++ src/index.js
@@ -1,41 +1,44 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import axios from "axios";
-
-
+import "./index.css"
 
 
 function QuoteGen() {
-    const [{text,author},Genquote]=useState();
-    useEffect(
-        axios({
-            method: 'get',
-            url: 'https://type.fit/api/quotes',
-            responseType: 'stream'
-          })
-            .then(function (response) {
-                var keys = Object.keys(response.data);
-                rndmkey = keys[Math.floor(Math.random() * keys.length)];
-               
-                    response.data[rndmkey].text;
-                    response.data[rndmkey].author;
-                
+  const [quote, Genquote] = useState({text: "I never see what has been done; I only see what remains to be done.",
+  author: "Marie Curie"});
+  const [quotes, Genquotes] = useState([]);
+function generate(){
+    
+    let rndmkey = [Math.floor(Math.random() * quotes.length)];
+    
+    Genquote(quotes[rndmkey])
+  } 
 
-            }),[]
-            
-    )
+
+  useEffect(()=>{axios.get("https://type.fit/api/quotes")
+      .then(function (response) {
+       Genquotes(response.data);
+        console.log(response.data);
+       
+        
+       });
+    
+  },[])
   return (
+    <>
+    <h1>Quote Generatore </h1>
     <div className="quote">
-     
       <blockquote>{quote.text}</blockquote>
       <span>{quote.author}</span>
-
-      <button onClick={()=>{Genquote( response.data[rndmkey].text, response.data[rndmkey].author)}}>
-        <img src="outline_sync_black_24dp.png" alt="" />
+      <button class="button-30" role="button" onClick={generate}>
+        Generate 
       </button>
     </div>
+    </>
+    
   );
 }
+    
 
 createRoot(document.getElementById("root")).render(<QuoteGen />);
-
