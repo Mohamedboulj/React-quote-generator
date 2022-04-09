3bd5c8b7d6688c860d270637ae7d9c677334d5d8
Mohamedboulj <med.blj93@gmail.com>
2022-04-09 22:39:30 +0000

    Style update

--- src/index.js
+++ src/index.js
@@ -1,57 +1,48 @@
 import React, { useEffect, useState } from "react";
 import { createRoot } from "react-dom/client";
 import axios from "axios";
-import "./index.css"
-
+import "./index.css";
 
 function QuoteGen() {
-  const [quote, Genquote] = useState({text: "Silence is the true friend that never betrays.",
-  author: "Confucius"});
+  const [quote, Genquote] = useState({
+    text: "Silence is the true friend that never betrays.",
+    author: "Confucius",
+  });
   const [quotes, Genquotes] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
-function generate(){
-    
+  function generate() {
     let rndmkey = [Math.floor(Math.random() * quotes.length)];
-    
-    Genquote(quotes[rndmkey])
-  } 
 
+    Genquote(quotes[rndmkey]);
+  }
 
-  useEffect(()=>{axios.get("https://type.fit/api/quotes")
-      .then(function (response) {
-        if(response.data.length > 0){
-          setIsLoading(false)
-          Genquotes(response.data);
-        }
-      
-        
-       
-        
-       });
-    
-  },[])
-  return isLoading?
-  <div class="loadingio-spinner-ripple-4pnogkvp7g3"><div class="ldio-ux3rtz5zf9">
-  <div></div><div></div>
-  </div></div>
-         :
-  
-  
-  (
-    <>
-    <h1>Quote Generatore </h1>
-    <div className="quote">
-      <blockquote>{quote.text}</blockquote>
-      <span>{quote.author}</span>
-      <button class="button-30" role="button" onClick={generate}>
-        Generate 
-      </button>
+  useEffect(() => {
+    axios.get("https://type.fit/api/quotes").then(function (response) {
+      if (response.data.length > 0) {
+        setIsLoading(false);
+        Genquotes(response.data);
+      }
+    });
+  }, []);
+  return isLoading ? (
+    <div class="loadingio-spinner-ripple-4pnogkvp7g3">
+      <div class="ldio-ux3rtz5zf9">
+        <div></div>
+        <div></div>
+      </div>
     </div>
+  ) : (
+    <>
+      <h1>Quote Generatore </h1>
+      <div className="quote">
+        <blockquote>{quote.text}</blockquote>
+        <span>{quote.author}</span>
+        <button class="button-30" role="button" onClick={generate}>
+          Generate
+        </button>
+      </div>
     </>
-    
   );
 }
-    
 
 createRoot(document.getElementById("root")).render(<QuoteGen />);
-
